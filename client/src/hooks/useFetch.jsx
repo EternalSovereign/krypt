7610eca160bea1fed20ff1b1cb604c9b2306a113
EternalSovereign <dipendrakumarbm5@gmail.com>
2024-03-02 17:40:18 +0530

    4th commit

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-const API_KEY = process.env.VITE_GIPHY_API;
+const API_KEY = import.meta.env.VITE_API_KEY;
 const useFetch = ({ keyword }) => {
     const [gifUrl, setGifUrl] = useState("");
     const fetchGifs = async () => {
         try {
             const response = await fetch(
-                `https://api.giphy.com/v1/gifs/search?api_key=${
-                    process.env.VITE_GIPHY_API
-                }&q=${keyword.split(" ").join("")}&limit=1`
+                `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword
+                    .split(" ")
+                    .join("")}&limit=1`
             );
             const { data } = await response.json();
 
