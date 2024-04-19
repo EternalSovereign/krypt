460bdb58a64fb407415e2c643b835c94d98d0608
EternalSovereign <dipendrakumarbm5@gmail.com>
2024-04-19 12:08:42 +0530

    2nd commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require(dotenv).config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.24",
     networks: {
         sepolia: {
-            url: "https://eth-sepolia.g.alchemy.com/v2/YXYNC4LKLRJboolonk00EaPwPPeR0Qx1",
-            accounts: [
-                "5a2aef93aed9e172537d5a25865a1b44d94038efcd39189371909d1c522bbaac",
-            ],
+            url: process.env.url,
+            accounts: [process.env.key],
         },
     },
 };
